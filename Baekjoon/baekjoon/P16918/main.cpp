507f095151642cac4c7a1e1c0289fb627fd38646
jieun <wldmsqkql@naver.com>
2020-09-17 13:28:31 +0900

    first commit

---
+++ Baekjoon/baekjoon/P16918/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  P16918
+//
+//  Created by 김지은 on 2020/09/17.
+//  Copyright © 2020 김지은. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
