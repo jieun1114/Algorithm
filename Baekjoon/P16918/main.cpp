40e3ed4f70ce6de03440428a1661a4924b0941f3
jieun <wldmsqkql@naver.com>
2020-09-17 13:34:55 +0900

    commit test

---
+++ Baekjoon/P16918/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  P16918
+//
+//  Created by 김지은 on 2020/09/17.
+//  Copyright © 2020 김지은. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    //sf
+    return 0;
+}
